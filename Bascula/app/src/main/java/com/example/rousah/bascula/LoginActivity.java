d7d519cb6084726b8e2eaea0ad48a00a0a5e1a69
Rosa <emvader@epsg.upv.es>
2018-11-24 17:28:35 +0100

    Update LoginActivity.java

--- Bascula/app/src/main/java/com/example/rousah/bascula/LoginActivity.java
+++ Bascula/app/src/main/java/com/example/rousah/bascula/LoginActivity.java
@@ -43,7 +43,7 @@ public class LoginActivity  extends AppCompatActivity {
                             new AuthUI.IdpConfig.GoogleBuilder().build())) //<<<<<<<<<<<<<
                     .setIsSmartLockEnabled(false)
                     .setTheme(R.style.LoginTheme)
-                    .setLogo(R.drawable.icon)
+                    .setLogo(R.drawable.logo_login)
                     .build(), RC_SIGN_IN);
         }
     }
