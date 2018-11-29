f2c47805a40cf2c562f97e7ebbcddf34dea9b1e8
Rosa <emvader@epsg.upv.es>
2018-11-29 10:38:14 +0100

    mqtt casafragment

--- Bascula/app/src/main/java/com/example/rousah/bascula/CasaFragment.java
+++ Bascula/app/src/main/java/com/example/rousah/bascula/CasaFragment.java
@@ -1,25 +1,34 @@
 package com.example.rousah.bascula;
 
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.BounceInterpolator;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.db.chart.animation.Animation;
-import com.db.chart.model.LineSet;
-import com.db.chart.model.Point;
-import com.db.chart.view.LineChartView;
+import com.example.comun.Mqtt;
 
-import java.text.DecimalFormat;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
-import static android.view.KeyCharacterMap.FULL;
+import static com.example.comun.Mqtt.broker;
+import static com.example.comun.Mqtt.clientId;
+import static com.example.comun.Mqtt.qos;
+import static com.example.comun.Mqtt.topicRoot;
+import static com.firebase.ui.auth.AuthUI.TAG;
 
 
 /**
@@ -30,34 +39,27 @@ import static android.view.KeyCharacterMap.FULL;
  * Use the {@link CasaFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class CasaFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
+public class CasaFragment extends Fragment implements MqttCallback {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private View view;
+
+    public Switch luces;
 
     private OnFragmentInteractionListener mListener;
 
-    private LineChartView grafica;
-    private Runnable mBaseAction;
+    //----------------MQTT---------------------
+    MqttClient client;
+    //----------------MQTT---------------------
 
     public CasaFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CasaFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static CasaFragment newInstance(String param1, String param2) {
         CasaFragment fragment = new CasaFragment();
         Bundle args = new Bundle();
@@ -74,49 +76,114 @@ public class CasaFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        //---------------MQTT---------------------
+        try {
+            Log.i(Mqtt.TAG, "Conectando al broker " + broker);
+            client = new MqttClient(broker, clientId, new MemoryPersistence());
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+            connOpts.setKeepAliveInterval(60);
+            connOpts.setWill(topicRoot+"WillTopic", "App desconectada".getBytes(),
+                    qos, false);
+            client.connect(connOpts);
+        } catch (MqttException e) {
+            Log.e(Mqtt.TAG, "Error al conectar.", e);
+        }
+
+        try {
+            Log.i(Mqtt.TAG, "Suscrito a " + topicRoot+"POWER");
+            client.subscribe(topicRoot+"POWER", qos);
+            client.setCallback(this);
+        } catch (MqttException e) {
+            Log.e(Mqtt.TAG, "Error al suscribir.", e);
+        }
+        //---------------MQTT---------------------
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        final View view = inflater.inflate(R.layout.casa, container, false);
+        view = inflater.inflate(R.layout.casa, container, false);
+        luces = view.findViewById(R.id.switchluces);
 
-        String[] labels = new String[2];
-        labels[0] = "12:00";
-        labels[1] = "13:00";
-        float[] values = new float[2];
-        values[0] = 20.0f;
-        values[1] = 30.0f;
+        // AQUI PONER CODIGO PARA LEER EL ESTADO MAS RECIENTE DEL SONOFF
+        // Y CAMBIAR EL ESTADO DEL SWITCH LUCES ACORDE CON EL ESTADO DEL
+        // SONOFF
 
-        LineSet dataset = new LineSet(labels, values);
-        dataset.addPoint(new Point("14:00", 40.0f));
 
-        dataset.setColor(Color.parseColor("#399699"))
-                .setDotsColor(Color.parseColor("#ff869b"))
-                .setThickness(4)
-                .setDashed(new float[]{10f, 10f});
+        // Este código crea un bucle discoteca encendiendo y apagando el sonoff sin parar
+       /* luces.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Log.w("switch: ", "pruebas");
+                if (isChecked) {
+                    try {
+                        Log.i(Mqtt.TAG, "Publicando mensaje: " + "encender sonoff");
+                        MqttMessage message = new MqttMessage("ON".getBytes());
+                        message.setQos(qos);
+                        message.setRetained(false);
+                        client.publish(topicRoot + "cmnd/POWER", message);
+                        Toast.makeText(getContext(), "Luces encendidas", Toast.LENGTH_SHORT).show();
+                    } catch (MqttException e) {
+                        luces.setChecked(false);
+                        Toast.makeText(getContext(), "Error", Toast.LENGTH_SHORT).show();
+                        Log.e(TAG, "Error", e);
+                    }
+                }
+                if (!isChecked) {
+                    try {
+                        Log.i(Mqtt.TAG, "Publicando mensaje: " + "apagar sonoff");
+                        MqttMessage message = new MqttMessage("OFF".getBytes());
+                        message.setQos(qos);
+                        message.setRetained(false);
+                        client.publish(topicRoot + "cmnd/POWER", message);
+                        Toast.makeText(getContext(), "Luces apagadas", Toast.LENGTH_SHORT).show();
+                    } catch (MqttException e) {
+                        luces.setChecked(true);
+                        Toast.makeText(getContext(), "Error", Toast.LENGTH_SHORT).show();
+                        Log.e(TAG, "Error", e);
+                    }
+                }
+            }
+        }); */
 
-        DecimalFormat formato = new DecimalFormat();
-        formato.applyPattern("#0.00");
-
-        grafica = view.findViewById(R.id.linechart);
-        grafica.addData(dataset);
-        grafica.setAxisColor(Color.parseColor("#399699"))
-                .setTypeface(Typeface.createFromAsset(getContext().getAssets(), "OpenSans-Semibold.ttf"))
-                .setLabelsFormat(formato);
-
-        grafica.show();
-
+        //---------------MQTT---------------------
+        luces.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (luces.isChecked()) {
+                    try {
+                        Log.i(Mqtt.TAG, "Publicando mensaje: " + "encender sonoff");
+                        MqttMessage message = new MqttMessage("ON".getBytes());
+                        message.setQos(qos);
+                        message.setRetained(false);
+                        client.publish(topicRoot + "cmnd/POWER", message);
+                        Toast.makeText(getContext(), "Luces encendidas", Toast.LENGTH_SHORT).show();
+                    } catch (MqttException e) {
+                        luces.setChecked(false);
+                        Toast.makeText(getContext(), "Error", Toast.LENGTH_SHORT).show();
+                        Log.e(TAG, "Error", e);
+                    }
+                }
+                if (!luces.isChecked()) {
+                    try {
+                        Log.i(Mqtt.TAG, "Publicando mensaje: " + "apagar sonoff");
+                        MqttMessage message = new MqttMessage("OFF".getBytes());
+                        message.setQos(qos);
+                        message.setRetained(false);
+                        client.publish(topicRoot + "cmnd/POWER", message);
+                        Toast.makeText(getContext(), "Luces apagadas", Toast.LENGTH_SHORT).show();
+                    } catch (MqttException e) {
+                        luces.setChecked(true);
+                        Toast.makeText(getContext(), "Error", Toast.LENGTH_SHORT).show();
+                        Log.e(TAG, "Error", e);
+                    }
+                }
+            }
+        });
+        //---------------MQTT---------------------
 
         // Inflate the layout for this fragment
         return view;
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
     }
 
     @Override
@@ -136,19 +203,55 @@ public class CasaFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+    @Override
+    public void connectionLost(Throwable cause) {
+        Log.d("Internet: ", cause.getMessage());
+        MainActivity estado = (MainActivity)getActivity();
+        Log.d("internet", cause.getMessage());
+        while (!((MainActivity)getActivity()).isNetworkAvailable()) {
+            Log.d(TAG, "Reintentando conexión MQTT");
+            try {
+                Log.i(Mqtt.TAG, "Conectando al broker " + broker);
+                client = new MqttClient(broker, clientId, new MemoryPersistence());
+                MqttConnectOptions connOpts = new MqttConnectOptions();
+                connOpts.setCleanSession(true);
+                connOpts.setKeepAliveInterval(60);
+                connOpts.setWill(topicRoot+"WillTopic", "App desconectada".getBytes(),
+                        qos, false);
+                client.connect(connOpts);
+            } catch (MqttException e) {
+                Log.e(Mqtt.TAG, "Error al conectar.", e);
+            }
+        }
+    }
+
+    @Override
+    public void messageArrived(String topic, MqttMessage message) throws Exception {
+        final String payload = new String(message.getPayload());
+        Log.d(TAG, "Recibiendo: " + topic + "->" + payload);
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                luces = view.findViewById(R.id.switchluces);
+                if (payload.contains("ON")) {
+                    luces.setChecked(true);
+                    Toast.makeText(getContext(), "Luces encendidas", Toast.LENGTH_SHORT).show();
+                }
+                if (payload.contains("OFF")) {
+                    luces.setChecked(false);
+                    Toast.makeText(getContext(), "Luces apagadas", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void deliveryComplete(IMqttDeliveryToken token) {
+        Log.d("ajjaajjaja", token.toString());
+    }
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
-
 }
